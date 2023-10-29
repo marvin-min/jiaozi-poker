50c6589afb9837d74fa7c7518a3bbb5a4427d930
Marvin.Min <xiaorong.min@gmail.com>
2023-10-29 22:56:49 +0800

    update layout

--- components/Poker/Poker.js
+++ components/Poker/Poker.js
@@ -1,17 +1,18 @@
 import PokerCard from '../PokerCard';
 import './poker.scss';
 const Poker = () => {
+  // 20, 40, 100, '∞',
   const storyPoints = [0, '1/2', 1, 2, 3, 5, 8, 13, '?', '☕️']
   const cards = storyPoints.map((storyPoint, index) => {
     return <PokerCard number={storyPoint} key={storyPoint} />
   })
   return (
-    <div class="flex flex-row gap-4 sm:gap-8 lg:gap-10 my-5">
-      <div class="basis-4/5 flex flex-row flex-wrap gap-4 py-5 bg-cyan-300 justify-around">
-        {cards}
-      </div>
-      <div class="basis-1/5 bg-pink-300">
+    <div class="flex flex-row gap-4 sm:gap-4 lg:gap-10 my-5">
+       <div class="basis-2/5  p-4 bg-white border border-gray-200 rounded">
         asfasdf
+      </div>
+      <div class="basis-3/5 flex flex-row flex-wrap gap-4 justify-around">
+        {cards}
       </div>
     </div>
   )
